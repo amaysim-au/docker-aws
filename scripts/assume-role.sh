46585c50d9c225b4751dac82669a7a7337d26e6c
Allan Denot <adenot@gmail.com>
2017-08-01 16:46:28 +1000

    Removing export so we can write to file

--- scripts/assume-role.sh
+++ scripts/assume-role.sh
@@ -5,16 +5,16 @@ unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN AWS_SECURITY_TOK
 # Old ec2 tools use other env vars
 unset AWS_ACCESS_KEY AWS_SECRET_KEY AWS_DELEGATION_TOKEN
 
-DURATION="${ROLE_DURATION:-3600}"
-SESSION_NAME="${ROLE_SESSION_NAME:-`hostname -s`}"
+DURATION="${AWS_ROLE_DURATION:-3600}"
+SESSION_NAME="${AWS_ROLE_SESSION_NAME:-`date +%s`}"
 
 # KST=access*K*ey, *S*ecretkey, session*T*oken
-KST=(`aws sts assume-role --role-arn "arn:aws:iam::$ACCOUNT_ID:role/$ROLE" \
+KST=(`aws sts assume-role --role-arn "arn:aws:iam::$AWS_ACCOUNT_ID:role/$AWS_ROLE" \
                           --role-session-name "$SESSION_NAME" \
                           --duration-seconds $DURATION \
                           --query '[Credentials.AccessKeyId,Credentials.SecretAccessKey,Credentials.SessionToken]' \
                           --output text`)
 
-echo "export AWS_ACCESS_KEY_ID='${KST[0]}'"
-echo "export AWS_SECRET_ACCESS_KEY='${KST[1]}'"
-echo "export AWS_SESSION_TOKEN='${KST[2]}'"
+echo "AWS_ACCESS_KEY_ID='${KST[0]}'"
+echo "AWS_SECRET_ACCESS_KEY='${KST[1]}'"
+echo "AWS_SESSION_TOKEN='${KST[2]}'"
